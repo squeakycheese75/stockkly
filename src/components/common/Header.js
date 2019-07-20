df38132b0c22c8838362bc8f52504df93b9559df
Jamie <james_wooltorton@hotmail.com>
2019-07-20 22:26:43 +0100

    Smaller icon file and updated packages

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styles from "./Header.css";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import logo from "./icon_256.png";
+import logo from "./icon_48.png";
 
 class Header extends Component {
   render() {
