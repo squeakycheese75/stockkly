61c5f82a7efe28ae85c3394db40ca9ffe9d3802e
Jamie <james_wooltorton@hotmail.com>
2019-08-12 10:10:40 +0100

    fotgot to remove header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -54,11 +54,11 @@ class Header extends Component {
                   <Button>About</Button>
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/transactions">
+              {/* <LinkContainer to="/transactions">
                 <Nav.Link>
                   <Button>Transactions</Button>
                 </Nav.Link>
-              </LinkContainer>
+              </LinkContainer> */}
             </Nav>
             <Nav pullright="true">
               {isAuthenticated() && (
