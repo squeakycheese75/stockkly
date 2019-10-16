910efa2279eaedb2aedc6d35f6bf56a649a4decc
Jamie <james_wooltorton@hotmail.com>
2019-10-16 14:21:31 +0100

    removed TestComponent

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -6,7 +6,6 @@ import ProfileForm from "./ProfileForm";
 import Loading from "../common/Loading";
 import { toast } from "react-toastify";
 import DevMode from "./DevMode";
-import TestComponent from "./TestComponent";
 
 function UserProfilePage({ loadProfile, saveProfile, history, ...props }) {
   const [profile, setProfile] = useState({ ...props.profile });
@@ -78,7 +77,6 @@ function UserProfilePage({ loadProfile, saveProfile, history, ...props }) {
         cancel={handleCancel}
       />
       <DevMode profile={profile} />
-      <TestComponent />
     </>
   );
 }
