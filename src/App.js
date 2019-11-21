0e49d297b1e1b4efd2670461ddc953e07303cb1f
Jamie <james_wooltorton@hotmail.com>
2019-11-21 18:59:53 +0000

    tweaks and starting footer

--- src/App.js
+++ src/App.js
@@ -179,7 +179,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div class="d-flex bottomArea">
       <div class="w-60">
-        <img src="images/carved-rock-logo-white.png" class="img-fluid h-25" />
+        {/* <img src="images" class="img-fluid h-25" /> */}
         <h5>About Us</h5>
         <p>
           Keelhaul aye Arr Corsair yardarm black jack careen rope's end overhaul
@@ -14,9 +14,9 @@ const Footer = () => {
       </div>
       <div class="d-flex flex-column">
         <h5>Contact Us:</h5>
-        <div>123 Jenkins St.</div>
-        <div>555-5555</div>
-        <div>info@stockkly.com</div>
+        <div>
+          <a href="mailto:info@stockkly.com">email</a>
+        </div>
       </div>
     </div>
   );
