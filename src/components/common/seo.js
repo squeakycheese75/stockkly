1bdc0d13c4cece3df33744f1b36d44435e6e4d73
Jamie <james_wooltorton@hotmail.com>
2020-02-06 13:17:23 +0000

    updating seo

--- src/components/common/seo.js
+++ src/components/common/seo.js
@@ -1,8 +1,8 @@
 export const seo = {
   title:
-    "Stockkly wealth tracker - Free Real-time Wealth Tracker (Crypto, Stocks, etc).",
+    "Stockkly wealth tracker - Free real-time Wealth Tracker for the modern portfolio. (Crypto, Stocks, etc).",
   description:
-    "A free, real-time, wealth-tracker that lets you track your portfolio of Stocks, Funds, Crypto, Fx, Gold, Silver and composites (FAANG) live!",
+    "A free, real-time, wealth tracker that lets you track your portfolio of Stocks, Funds, Crypto, Fx, Gold, Silver and composites (FAANG) live!",
   url: "https://stockkly.com/",
   image: "https://stockkly.com/images/stockkly.png"
 };
