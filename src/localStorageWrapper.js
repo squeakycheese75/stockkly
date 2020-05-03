20abffa6324adf9fb3d0809d327a930aa0292f5b
Jamie <james_wooltorton@hotmail.com>
2020-05-03 17:58:07 +0100

    Updated wrapper to mock in a function when not running on the client

--- src/localStorageWrapper.js
+++ src/localStorageWrapper.js
@@ -1,20 +1,25 @@
-// export const storage = () => {
-//   if (!typeof window.localStorage === "undefined") return window.localStorage;
-//   else if (!typeof localStorage === "undefined") return localStorage;
-//   else return false;
-// };
-
-const storage = () => {
-  // if (!typeof window.localStorage === "undefined") return window.localStorage;
-  // else if (!typeof localStorage === "undefined") return localStorage;
-  // else return false;
-  return localStorage;
+const localStorageMock = function () {
+  var store = {};
+  return {
+    getItem: function (key) {
+      return store[key];
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString();
+    },
+    clear: function () {
+      store = {};
+    },
+    removeItem: function (key) {
+      delete store[key];
+    },
+  };
 };
 
-// const store = function (_store) {
-//   return function () {
-//     return localStorage;
-//   };
-// };
+const storage = () => {
+  if (!typeof window.localStorage === "undefined") return window.localStorage;
+  else if (!typeof localStorage === "undefined") return localStorage;
+  else return localStorageMock();
+};
 
 module.exports = { storage };
