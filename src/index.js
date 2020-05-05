1147ba8d20ca24d9c4545fdee8e3bae5aa7748e9
Jamie <james_wooltorton@hotmail.com>
2020-05-05 08:22:58 +0100

    fixed login bug

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { render } from "react-snapshot";
+import React from 'react';
+import { render } from 'react-snapshot';
 // import {  render } from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
-import configureStore from "./redux/configureStore";
-import { Provider as ReduxProvider } from "react-redux";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
 
 const store = configureStore();
 
 render(
   <ReduxProvider store={store}>
     <Router>
-      {/* <Route component={App} /> */}
-      <App />
+      <Route component={App} />
     </Router>
   </ReduxProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
