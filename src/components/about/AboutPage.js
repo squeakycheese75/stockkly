ceac440ee578ce6a032788c7017728699cd9219a
Jamie <james_wooltorton@hotmail.com>
2020-05-05 08:20:35 +0100

    Lint re-factor working

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import styles from "./AboutPage.css";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import styles from './AboutPage.css';
 
 const AboutPage = () => {
   return (
     <div className={styles}>
       <Card border="dark">
-        <Card.Header as="h5">What's new in version 0.2</Card.Header>
+        <Card.Header as="h5">What&apos;s new in version 0.2</Card.Header>
         <Card.Body>
           <p>
             Added new Wallet function - including support for transactions and
@@ -26,19 +26,19 @@ const AboutPage = () => {
           {/* <Card.Title>A little bit about the developer...</Card.Title> */}
           {/* <Card.Text> */}
           <p>
-            I'm a freelance software engineer and digital nomad. Currently
+            I&apos;m a freelance software engineer and digital nomad. Currently
             living and working from...<s>London</s>, <s>Berlin</s>,
             <s>Barcelona</s>, <s>Chamonix</s>, <s>Devon</s>, <s>Madrid</s>,
-            <s>Santiago</s>, <s>Lima</s>, <s>Toronto</s>, <s>Coimbra</s>,{" "}
+            <s>Santiago</s>, <s>Lima</s>, <s>Toronto</s>, <s>Coimbra</s>,{' '}
             <s>Aviero</s>, <s>Vancouver</s>, <s> London</s>, <s>Berlin</s>,
             Edinburgh.
           </p>
           <p>
             I built Stockkly as I wanted a mobile friendly, one-stop source for
-            watching prices that I'm interested in.
+            watching prices that I&apos;m interested in.
           </p>
           <p>
-            Feel free to <a href="mailto:james_wooltorton@hotmail.com">email</a>{" "}
+            Feel free to <a href="mailto:james_wooltorton@hotmail.com">email</a>{' '}
             me with feedback/comments/suggestions.
           </p>
           <p>
@@ -47,7 +47,7 @@ const AboutPage = () => {
             GitHub.
           </p>
           <p>
-            {" "}
+            {' '}
             <a href="https://github.com/squeakycheese75">
               <img
                 src="./images/github.png"
@@ -55,7 +55,7 @@ const AboutPage = () => {
                 height="30"
                 className="d-inline-block align-top"
                 alt="GitHub"
-              />{" "}
+              />{' '}
               GitHub
             </a>
           </p>
---
+++ src/components/t.js
@@ -0,0 +1,4 @@
+const express = require('express');
+
+var x = "1"
+
